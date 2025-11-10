f09ad6ecf22e992aaa53382039f7a21eb588c796
faal <130977909+faal2@users.noreply.github.com>
2025-11-10 15:34:40 +0100

    Skapade huvudmenyn

--- ExerciseTwo/Program.cs
+++ ExerciseTwo/Program.cs
@@ -4,7 +4,18 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("""
+            Välkommen till huvudmenyn.
+            Här har du följande handlingar du kan utföra. Du interagerar genom att skriva in siffran till det alternativ du vill göra:
+
+            0. För att avsluta programmet.
+            1. Se priserna för biobiljetter.
+            2. Beräkna totalpris för biobiljetter.
+            3. Repeterad text (10 gånger). 
+            4. Tredje ordet från en text. 
+
+            Skriv en siffra:  
+            """);
         }
     }
 }
